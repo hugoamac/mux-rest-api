822e6b6882db8b5c6e2278dff5433db45fa82d84
hugo machado <hugoamac@live.com>
2021-04-04 00:53:17 -0300

    doc(): change docuemntation for contact service

--- domain/contact/service.go
+++ domain/contact/service.go
@@ -1,6 +1,6 @@
 package contact
 
-//ContactService - This type provides the contact service interface
+//ContactService - This interface provides the object with the responsibility to perform contact domain services.
 type ContactService interface {
 	Create(c *ContactEntity) error
 	List() ([]*ContactEntity, error)
@@ -9,37 +9,37 @@ type ContactService interface {
 	Remove(id string) error
 }
 
-//contactService -This struct provides the contact service object
+//contactService - This struct provides the implemtation for ContactService interface.
 type contactService struct {
 	repository ContactRepository
 }
 
-//NewContactService - This method provides the instance of ContactService
+//NewContactService - This method provides the instance of ContactService interface.
 func NewContactService(repository ContactRepository) ContactService {
 	return &contactService{repository}
 }
 
-//Create - This method provides the created a new contact
+//Create - This method provides the creation of a new contact.
 func (s *contactService) Create(c *ContactEntity) error {
 	return s.repository.Create(c)
 }
 
-//ListAll - This method provides the list of all contacts
+//ListAll - This method provides to retrieve the contact list.
 func (s *contactService) List() ([]*ContactEntity, error) {
 	return s.repository.FetchAll()
 }
 
-//FindById - This method provides get the contact by id
+//FindById - This method provides to retrieve a contact by the identifier.
 func (s *contactService) FindById(id string) (*ContactEntity, error) {
 	return s.repository.Find(id)
 }
 
-//Update - This method provides update the contact
+//Update - This method provides for updating contact data.
 func (s *contactService) Update(c *ContactEntity) (*ContactEntity, error) {
 	return s.repository.Update(c)
 }
 
-//Remove - This method provides removed the contract
+//Remove - This method provides to remove a contact by the identifier.
 func (s *contactService) Remove(id string) error {
 	return s.repository.Delete(id)
 }
