95d7481415f201f585feca24636cba69be0a2000
hugo machado <hugoamac@live.com>
2021-04-04 02:41:28 -0300

    feat(): created loadVars from env file configuration or variable environments

--- infra/load_vars.go
+++ infra/load_vars.go
@@ -2,9 +2,21 @@ package infra
 
 import "github.com/spf13/viper"
 
+type Config struct {
+	MongoUri    string `mapstructure:"MONGO_URI"`
+	MongoDbName string `mapstructure:"MONGO_DBNAME"`
+	AppPort     string `mapstructure:"APP_PORT"`
+}
+
 // LoadVars - This method provides the loading of the environment variables.
-func LoadVars() {
+func LoadVars() (config Config, err error) {
 
 	viper.SetConfigFile(".env")
-	viper.ReadInConfig()
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
 }
