26517b11d22d9a74b76fda9c06f1597fc1de31d3
hugo machado <hugoamac@live.com>
2021-04-04 00:49:27 -0300

    doc(): change documentation for connection interface

--- infra/connection.go
+++ infra/connection.go
@@ -10,47 +10,48 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-//Connection - This interface provides the connection database objection
-type Connection interface{
-	DB()*mongo.Database
+//Connection - This interface provides the object with the responsibility to create a connection to the mongodb database.
+type Connection interface {
+	DB() *mongo.Database
 	Close()
 }
 
-//conn - This struct type provides the connection from mongo.Database
-type conn struct{
+//conn - This struct provides the implementation for Connection interface.
+type conn struct {
 	db *mongo.Database
 }
 
-// Connect - This method provides the instance of mongo client connect
-func NewConnection()Connection{
+// Connect - This method provides the instance of Connection interface.
+func NewConnection() Connection {
 
-	mongo_uri:= viper.Get("MONGO_URI").(string)
-	mongo_dbname:= viper.Get("MONGO_DBNAME").(string)
+	mongo_uri := viper.Get("MONGO_URI").(string)
+	mongo_dbname := viper.Get("MONGO_DBNAME").(string)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongo_uri))
-	if err != nil{
+	if err != nil {
 		log.Fatal("Error on connected mongo db")
 	}
 
-	db:=client.Database(mongo_dbname)
+	db := client.Database(mongo_dbname)
 
-	log.Println("Mongo connected successfully")	
+	log.Println("Mongo connected successfully")
 	return &conn{db}
 }
 
-//DB - This method provides the instance of mongo client database
-func( c *conn)DB()*mongo.Database{
+//DB - This method provides the instance of connection to the mongodb database.
+func (c *conn) DB() *mongo.Database {
 	return c.db
 }
-//Close -  This method provides the disconnect from mongo database
-func( c *conn)Close(){
+
+//Close - This method provides to close the connection to the mongodb database.
+func (c *conn) Close() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	log.Println("Mongo disconnected successfully")	
-	c.db.Client().Disconnect(ctx)	
+	log.Println("Mongo disconnected successfully")
+	c.db.Client().Disconnect(ctx)
 }
