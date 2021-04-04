8ceadce4a3c10e9a0f486d18270e382628f202cf
hugo machado <hugoamac@live.com>
2021-04-04 01:05:40 -0300

    refactor(): add routes for handlers contact

--- web/main.go
+++ web/main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"mux-rest-api/infra"
+	"mux-rest-api/web/routes"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
 )
 
 func main() {
 
-	fmt.Println("started application...")
+	log.Println("started application...")
+
 	infra.LoadVars()
 
+	port := viper.Get("APP_PORT").(string)
+
+	conn := infra.NewConnection()
+	defer conn.Close()
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	//routes
+	routes.ContactRoutes(conn.DB(), router)
+
+	logger := log.New(os.Stderr, "Logger", log.Lshortfile)
+	server := &http.Server{
+		Addr:         port,
+		Handler:      router,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		ErrorLog:     logger,
+	}
+
+	log.Printf("server listening at port%s", port)
+
+	log.Fatal(server.ListenAndServe())
 }
